eeeabcb1e42e930047024d246613c55a5a596c6d
Programmer <aceraspire315510@gmail.com>
2024-12-14 18:42:30 +0500

    version 1.8

--- loader.py
+++ loader.py
@@ -4,6 +4,8 @@ from data.config import BOT_TOKEN
 from aiogram.fsm.storage.memory import MemoryStorage
 from utils.database_manager.sqlite import Database
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import os
+os.environ['TZ'] = 'Asia/Tashkent'
 bot = Bot(token=BOT_TOKEN,parse_mode='HTML')
 dp = Dispatcher(storage=MemoryStorage())
 db = Database(path_to_db='data/main.db')
